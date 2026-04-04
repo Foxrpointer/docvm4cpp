00e413dc9c54c6af53507210083d839cfaba4fb5
Kyuujuu <foxrptr@163.com>
2026-04-04 21:31:43 +0800

    commit

---
+++ grammer.md
@@ -0,0 +1,40 @@
+## 一些关于语法的伪代码
+
+### v0.0.1
+```c++
+#include "docvm.h"
+#include "basebtn.h"//假设是一个控件库
+
+int main()
+{
+    // new vm 对象
+    docvm::VM mechine = new docvm::vm();
+    mechine
+    .setGraphisEngine(gl)//gl是个enum
+    .is_GC(false)//是否自动为成员管理内存
+    .debug(true);
+
+    // 引入控件库
+    ctrl = basebtn::init();
+    if (!ctrl) return -1;
+
+    // 导出对象
+    docvm::Viewport& view;
+    mechine.export_viewport(&view);
+
+    // 设置属性
+    view
+    .setViewportSize(800, 600);
+
+    // canva
+    view.canva
+    .button(100, 100)
+    .text("你好")
+    .save();
+
+    mechine.init(); // 卡在这里
+    mechine.terminate();
+
+    return 0;
+}
+```
--- src/example/sea-ocean-render.cpp
+++ src/example/sea-ocean-render.cpp
@@ -3,41 +3,46 @@
 
 void render_prepare()
 {
-    /// 顶点输入
-    /// 逆时针定义顶点
-    /// 渲染的时候逆时针渲染。可以被判断为正面
-    /// 背面会被OpenGL当成看不到的面剔除...
-    float vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-     0.0f,  0.5f, 0.0f
+    /// 顶点输入，逆时针输入。XYZRGB
+    float vertecs[] = {
+    -0.5f, -0.5f, 0.0f, -0.5f, -0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f,
+    0.0f,  0.5f, 0.0f, 0.0f,  0.5f, 0.0f
     };
 
-    /// 生成 VBO 对象 Vertex Buffer Object
-    /// n是数量，buffers是缓冲区的句柄
-    /// VBO是句柄，不是指针
-    GLuint VBO;
-    glGenBuffers(1, &VBO);
+    // 创建VBO。1生成2绑定3赋值
+    GLuint posotion_and_color_VBO;
+    glGenBuffers(1, &posotion_and_color_VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, posotion_and_color_VBO);
+    // 把顶点数据上传到显存
+    // GL_STATIC_DRAW表示数据只上传一次，多次绘制
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertecs), vertecs, GL_STATIC_DRAW);
 
-    /// 绑定VBO到当前活跃的ARRAY_BUFFER
-    /// 后续所有 gl*Buffer* 操作默认作用于该 VBO
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-    /// 把顶点数据上传到显存
-    /// GL_STATIC_DRAW表示数据只上传一次，多次绘制
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    /// GL_ARRAY_BUFFER 必须和VBO一样 ↑
-
+    // 创建和绑定VAO。1生成2绑定3启用槽位4赋值
+    GLuint VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, posotion_and_color_VBO);
+    // 位置的VAO属性
+    glEnableVertexAttribArray(0);
     glVertexAttribPointer(
-    0,                  // attribute 0（对应 layout(location=0)）
-    3,                  // 每个顶点 3 个分量 (x,y,z)
-    GL_FLOAT,           // 数据类型
-    GL_FALSE,           // 不归一化
-    3 * sizeof(float),  // 步长（stride）= 12 字节
-    (void*)0            // 起始偏移
+        0,  // 指定0号位置
+        3,  // 一个属性3个单位
+        GL_FLOAT,   // 那3个单位是float
+        GL_FALSE,   // 不归一化
+        3*sizeof(float),    // 步长12bytes
+        (void* )0   // 无偏移量
     );
-    glEnableVertexAttribArray(0);  // 必须启用！默认禁用
-    /// 顶点着色器
+    // 颜色的VAO属性
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(
+        1,  // 指定1号位置
+        3,  // 一个属性3个单位
+        GL_FLOAT,   // 那3个单位是float
+        GL_FALSE,   // 不归一化
+        3*sizeof(float),    // 步长12bytes
+        (void* )(3*sizeof(float))   // 偏移量为3个float
+    );
 }
 
 void render_func()
